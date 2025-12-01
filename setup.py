0c97427b8cbd66d509a3cc24747eeea8b157779d
Thierry Moudiki <thierry.moudiki@gmail.com>
2025-12-01 11:58:09 +0100

    Remove Cython installation from setup.py
    Removed the installation of Cython during setup.

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-subprocess.check_call(['pip', 'install', 'Cython'])
-
 __version__ = "0.9.0"
 
 here = path.abspath(path.dirname(__file__))
